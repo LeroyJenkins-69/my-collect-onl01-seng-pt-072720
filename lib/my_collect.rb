d6cae2a26be41ab3f5577e52f7cf1010d7f67ae3
Kordell Parrish <parrishkordell@gmail.com>
2020-08-03 21:56:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array)
   i = 0 
   array2 = []
-  array2 << while i < array.length
-  yield(array[i])
+  while i < array.length
+  array2 << yield(array[i])
   i += 1
   end
   array2
